bb354759d04b177713b1581a7130ec888a359a6c
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-07 06:56:02 -0400

    Added Header component to show title of page and instruction

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 import './App.css';
 
 function App(){
   return (
   <div className="App">
     <Wrapper>
-      
+      <Header />
     </Wrapper>
   </div>
   );
