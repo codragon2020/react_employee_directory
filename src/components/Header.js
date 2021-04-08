f0847ef5a907378a6abc2c52eac05fd71e1a1bac
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-08 07:33:09 -0400

    Added styling to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-//import "../styles/Header.css";
+import "../styles/Header.css";
 
 export default class Header extends Component {
     render(){
         return (
             <div className="header">
                 <h1>Employee Directory</h1>
-                <p>Maybe add filter instructions here???</p>
+                <p>Click Name to sort by Name or use the Search box to filter your results!</p>
             </div>
         )
     }
