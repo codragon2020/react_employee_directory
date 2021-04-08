7ab529f9325411c8a89b1dda2c40179b04ebd7eb
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-08 06:26:31 -0400

    Added SearchBox component and updated Nav to handle it

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import React from "react";
+import SearchBox from "./SearchBox.js";
 
 function Nav({ handleSearchChange }) {
     return (
         <nav className = "navbar navbar-expand navbar-light bg-light">
-            <div className="navbar-collpase row" id="navbarNav"></div>
+            <div className="navbar-collpase row" id="navbarNav">
+                <SearchBox handleSearchChange={handleSearchChange}/>
+            </div>
         </nav>
     );
 }
