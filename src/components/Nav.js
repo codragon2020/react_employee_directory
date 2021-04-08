d5cbee3e612c40fee37263d873f71ff98c670288
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-08 06:17:12 -0400

    Added Nav component for search box

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav({ handleSearchChange }) {
+    return (
+        <nav className = "navbar navbar-expand navbar-light bg-light">
+            <div className="navbar-collpase row" id="navbarNav"></div>
+        </nav>
+    );
+}
+
+export default Nav;
