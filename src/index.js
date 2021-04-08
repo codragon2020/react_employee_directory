f522b868de0cab3210d34e2605a635d0e79195a8
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-08 07:04:45 -0400

    Added Bootstrap and FontAwesome for light styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"></link>
 
 ReactDOM.render(
   <React.StrictMode>
